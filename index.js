4414c30668a9e4586bce3cff57fa4e8991c6abbf
aditya24-hum <adityagupta32007@gmail.com>
2026-03-28 22:09:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,19 @@
 /**
- * @fileoverview Package exports for @eslint/eslintrc
- * @author Nicholas C. Zakas
+ * @fileoverview `ConfigArray` class.
+ * @author Toru Nagashima <https://github.com/mysticatea>
  */
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
 
-import {
-    ConfigArrayFactory,
-    createContext as createConfigArrayFactoryContext
-} from "./config-array-factory.js";
+import { ConfigArray, getUsedExtractedConfigs } from "./config-array.js";
+import { ConfigDependency } from "./config-dependency.js";
+import { ExtractedConfig } from "./extracted-config.js";
+import { IgnorePattern } from "./ignore-pattern.js";
+import { OverrideTester } from "./override-tester.js";
 
-import { CascadingConfigArrayFactory } from "./cascading-config-array-factory.js";
-import * as ModuleResolver from "./shared/relative-module-resolver.js";
-import { ConfigArray, getUsedExtractedConfigs } from "./config-array/index.js";
-import { ConfigDependency } from "./config-array/config-dependency.js";
-import { ExtractedConfig } from "./config-array/extracted-config.js";
-import { IgnorePattern } from "./config-array/ignore-pattern.js";
-import { OverrideTester } from "./config-array/override-tester.js";
-import * as ConfigOps from "./shared/config-ops.js";
-import ConfigValidator from "./shared/config-validator.js";
-import * as naming from "./shared/naming.js";
-import { FlatCompat } from "./flat-compat.js";
-import environments from "../conf/environments.js";
-
-//-----------------------------------------------------------------------------
-// Exports
-//-----------------------------------------------------------------------------
-
-const Legacy = {
+export {
     ConfigArray,
-    createConfigArrayFactoryContext,
-    CascadingConfigArrayFactory,
-    ConfigArrayFactory,
     ConfigDependency,
     ExtractedConfig,
     IgnorePattern,
     OverrideTester,
-    getUsedExtractedConfigs,
-    environments,
-
-    // shared
-    ConfigOps,
-    ConfigValidator,
-    ModuleResolver,
-    naming
-};
-
-export {
-
-    Legacy,
-
-    FlatCompat
-
+    getUsedExtractedConfigs
 };
